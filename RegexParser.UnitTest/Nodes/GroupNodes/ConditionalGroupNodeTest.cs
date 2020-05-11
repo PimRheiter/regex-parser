3728af139c7b6583ee630281d2e42baed5396f0f
Pim Rheiter <Pim.Rheiter@infosupport.com>
2020-05-11 13:32:06 +0200

    Add Condition and Alternates properties. Don't allow creation of ConditionalGroupNode without condition outside of assembly.

--- RegexParser.UnitTest/Nodes/GroupNodes/ConditionalGroupNodeTest.cs
+++ RegexParser.UnitTest/Nodes/GroupNodes/ConditionalGroupNodeTest.cs
@@ -10,21 +10,98 @@ namespace RegexParser.UnitTest.Nodes.GroupNodes
     public class ConditionalGroupNodeTest
     {
         [TestMethod]
-        public void ToStringShouldReturnConditionalGroupWithConditionAndThenAndElseStatements()
+        public void ToStringShouldReturnConditionalGroupWithConditionAndThenAndElseAlternates()
         {
             // Arrange
             var thenBranch = new CharacterNode('t');
             var elseBranch = new CharacterNode('e');
-            var branches = new AlternationNode(new List<RegexNode> { thenBranch, elseBranch });
+            var alternates = new AlternationNode(new List<RegexNode> { thenBranch, elseBranch });
             var condition = new CaptureGroupNode(new CharacterNode('c'));
-            var childNodes = new List<RegexNode> { condition, branches };
-            var target = new ConditionalGroupNode(childNodes);
+            var target = new ConditionalGroupNode(condition, alternates);
 
             // Act
             var result = target.ToString();
 
             // Assert
             result.ShouldBe("(?(c)t|e)", result);
+        }
+
+        [TestMethod]
+        public void ToStringOnConditionalWithConcatenationAsAlternatesShouldReturnConditionalGroupWithConditionAndOnlyThenAlternate()
+        {
+            // Arrange
+            var thenBranchChildNodes = new List<RegexNode> { new CharacterNode('t'), new CharacterNode('h'), new CharacterNode('e'), new CharacterNode('n') };
+            var alternates = new ConcatenationNode(thenBranchChildNodes);
+            var condition = new CaptureGroupNode(new CharacterNode('c'));
+            var target = new ConditionalGroupNode(condition, alternates);
+
+            // Act
+            var result = target.ToString();
+
+            // Assert
+            result.ShouldBe("(?(c)then)", result);
+        }
+
+        [TestMethod]
+        public void ToStringOnConditionalWithoutAlternatesShouldReturnConditionalGroupWithOnlyCondition()
+        {
+            // Arrange
+            var condition = new CaptureGroupNode(new CharacterNode('c'));
+            var target = new ConditionalGroupNode(condition);
+
+            // Act
+            var result = target.ToString();
+
+            // Assert
+            result.ShouldBe("(?(c))", result);
+        }
+
+        [TestMethod]
+        public void ConditionShouldReturnOriginalCondition()
+        {
+            // Arrange
+            var thenBranch = new CharacterNode('t');
+            var elseBranch = new CharacterNode('e');
+            var alternates = new AlternationNode(new List<RegexNode> { thenBranch, elseBranch });
+            var condition = new CaptureGroupNode(new CharacterNode('c'));
+            var target = new ConditionalGroupNode(condition, alternates);
+
+            // Act
+            var result = target.Condition;
+
+            // Assert
+            result.ShouldBe(condition);
+        }
+
+        [TestMethod]
+        public void AlternatesShouldReturnOriginalAlternates()
+        {
+            // Arrange
+            var thenBranch = new CharacterNode('t');
+            var elseBranch = new CharacterNode('e');
+            var alternates = new AlternationNode(new List<RegexNode> { thenBranch, elseBranch });
+            var condition = new CaptureGroupNode(new CharacterNode('c'));
+            var target = new ConditionalGroupNode(condition, alternates);
+
+            // Act
+            var result = target.Alternates;
+
+            // Assert
+            result.ShouldBe(alternates);
+        }
+
+        [TestMethod]
+        public void AlternatesShouldReturnNullIfNoAlternates()
+        {
+            // Arrange
+            var condition = new CaptureGroupNode(new CharacterNode('c'));
+            var target = new ConditionalGroupNode(condition);
+
+            // Act
+            var result = target.Alternates;
+
+            // Assert
+            result.ShouldBeNull(); ;
         }
     }
 }
--- RegexParser/Nodes/GroupNodes/ConditionalGroupNode.cs
+++ RegexParser/Nodes/GroupNodes/ConditionalGroupNode.cs
@@ -1,26 +1,32 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace RegexParser.Nodes.GroupNodes
 {
     /// <summary>
     /// RegexNode representing a conditional group "(?(condition)then|else)".
-    /// A ConditionalGroupNode should have exactly two children.
+    /// A ConditionalGroupNode should have exactly at least one child.
     /// The first child is the condition and should be a GroupNode.
-    /// The second child is should be an AlternationNode with exactly two children.
-    /// The first alternate is the "then" branch.
-    /// The second alternate is the "else" branch.
+    /// A ConditionalGroupNode should have at most two children.
+    /// The second child is should be an AlternationNode with exactly two children or a ConcatenationNode.
+    /// If the second child is an AlternationNode, the first alternate is the "then" branch, the second alternate is the "else" branch.
+    /// If the second child is a ConcatenationNode, the concatenation is the "then" branch, the "else" branch will implicitly match an empty string.
+    /// If there is no second child, both the "then" and "else" braches will implicitly match an empty string.
     /// </summary>
     public class ConditionalGroupNode : GroupNode
     {
-        public ConditionalGroupNode() { }
+        public RegexNode Condition => ChildNodes.First();
+        public RegexNode Alternates => ChildNodes.ElementAtOrDefault(1);
 
-        public ConditionalGroupNode(RegexNode childNode)
-            : base(childNode)
+        internal ConditionalGroupNode() { }
+
+        public ConditionalGroupNode(RegexNode condition)
+            : base(condition)
         {
         }
 
-        public ConditionalGroupNode(IEnumerable<RegexNode> childNodes)
-            : base(childNodes)
+        public ConditionalGroupNode(RegexNode condition, RegexNode alternates)
+            : base(new List<RegexNode> { condition, alternates })
         {
         }
 
